4e237ee9f930120f702a9ad32a6c074f12a71cda
Priyanshu <priyanshubaliyan494@gmail.com>
2026-05-15 13:09:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "17abc"
+// console.log(typeof score);
+// console.log(score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" --> 33
+// "33ab" --> NaN 
+// true --> 1; false --> 0;
+// undefined --> NaN
+// null --> 0
+
+
+let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// "" --> false
+// "Value" --> true
+// 1 --> true
+// 0 --> false
+
+
+let anyNumber = 123
+let stringAnyNumber = String(anyNumber);
+
+console.log(typeof stringAnyNumber);
+console.log (stringAnyNumber);
+

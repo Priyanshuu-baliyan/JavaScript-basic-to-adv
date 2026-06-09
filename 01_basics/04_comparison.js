bcaa443b72731569ec1213b0f1db5ef0d5415b51
Priyanshu <priyanshubaliyan494@gmail.com>
2026-06-09 12:30:55 +0000

    Comparison of datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 < 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log(2 > "1");
+
+// console.log(null > 0); // true because >= convert the null to a number, treating it as 0.
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null === 0); // strict check
+// console.log(null <= 0); // true
+
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+// console.log(undefined === 0);
+// console.log(undefined <= 0);
+
+// ===
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+
+
